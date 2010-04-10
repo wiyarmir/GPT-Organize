d20bd60d40051d16c27f760880e849a810cad682
Guillermo Orellana Ruiz <wiyarmir@gmail.com>
2010-04-10 16:41:00 +0200

    More apropiate naming Signed-off-by: Guillermo Orellana Ruiz <wiyarmir@gmail.com>

---
+++ src/gptorganize/Main.java
@@ -0,0 +1,17 @@
+package gptorganize;
+
+import gptorganize.base.GPTOrganize;
+import java.io.IOException;
+
+
+
+/**
+ *
+ * @author Guillermo
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        GPTOrganize.test();
+    }
+}
