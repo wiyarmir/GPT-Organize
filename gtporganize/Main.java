cf6db5d1f27bd74ace893a0f9a08fd7c749a4e27
Guillermo Orellana Ruiz <wiyarmir@gmail.com>
2010-07-23 12:19:31 +0200

    Cleaning up
    Signed-off-by: Guillermo Orellana Ruiz <wiyarmir@gmail.com>

---
+++ gtporganize/Main.java
@@ -0,0 +1,17 @@
+package gtporganize;
+
+import gtporganize.base.GTPOrganize;
+import java.io.IOException;
+
+
+
+/**
+ *
+ * @author Guillermo
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        GTPOrganize.test();
+    }
+}
